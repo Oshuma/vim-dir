1a6a3e329ce109e5a55064b10ce23c19b5c92c8e
Dale Campbell <dale.campbell@escapemg.com>
2011-04-26 02:20:01 -0400

    clean before make

--- install.sh
+++ install.sh
@@ -25,5 +25,5 @@ git clone http://github.com/gmarik/vundle.git $vim_dir/vundle.git
 
 # Install the bundles and compile plugin extentions.
 # Note: We have to use a vim '-c' hack here, since the script exists when vim does.
-compile_extentions="cd $vim_dir/bundle/Command-T/ruby/command-t && ruby extconf.rb && make"
+compile_extentions="cd $vim_dir/bundle/Command-T/ruby/command-t && ruby extconf.rb && make clean && make"
 vim -c :BundleInstall -c ":!$compile_extentions" -c :q
