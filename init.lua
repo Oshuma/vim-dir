90d6796a4fa19de1ac96abcdcd6d74002f44fbfb
Dale Campbell <mail@dale.camp>
2024-03-06 13:31:06 -0500

    update

--- init.lua
+++ init.lua
@@ -152,6 +152,7 @@ local lspconfig = require('lspconfig')
 lspconfig.rust_analyzer.setup({})
 lspconfig.gopls.setup({})
 lspconfig.pyright.setup({})
+lspconfig.ruby_ls.setup({})
 
 vim.keymap.set("n", "<Leader>h", vim.diagnostic.open_float)
 -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
